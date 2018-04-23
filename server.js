fcbd880c326d110f8de916dd1046f5269e78bad6
Jan Montizaan <janmontizaan@gmail.com>
2018-04-23 14:46:25 +0200

    ready to start

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.all('*', (req, res, next) => {
     next();
 });
 
+
+
+app.get('/hello', (req, res, next) => {
+    res.status(200).json({
+        'msg': 'Hello JEDI\'s'
+    })
+});
+
 app.listen(port, () => {
     console.log('the magic happens at localhost: ' + port);
 });
